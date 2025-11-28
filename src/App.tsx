1f1fac8b78a8ae677507b4b7793ee435067451ca
sufiyan <46070544+sufiyan2506@users.noreply.github.com>
2025-11-28 15:33:24 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster.tsx";
+import { Toaster as Sonner } from "@/components/ui/sonner.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
